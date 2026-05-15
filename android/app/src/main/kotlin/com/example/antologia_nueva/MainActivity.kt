c5a2c1ced97e12b228e6ce210b5763408dab0f42
jeremyt <jeremytcool@gmail.com>
2026-05-14 21:48:50 -0500

    ya esta es

--- android/app/src/main/kotlin/com/example/antologia_nueva/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/antologia_nueva/MainActivity.kt
@@ -1,5 +1,13 @@
 package com.example.antologia_nueva
 
+import android.view.WindowManager
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // ESTA LÍNEA PROTEGE TODA LA APP (MODO CAOS SEGURO)
+        window.addFlags(WindowManager.LayoutParams.FLAG_SECURE)
+    }
+}
